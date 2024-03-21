25c00ec98164b6f030a0c70e4c8ef37016ed9142
Андрей <andreiyarmolchuk@yandex.ru>
2024-03-21 13:50:06 +0400

    - Создать метод который будет считать площадь квадрата с произвольными (установленными вами) сторонами - Создать метод который будет считать площадь круга c произвольным радиусом - Создать метод который будет считать площадь треугольника произвольными (установленными вами) сторонами

--- HomeWork1/src/main/java/vd/lesson1/Main.java
+++ HomeWork1/src/main/java/vd/lesson1/Main.java
@@ -6,6 +6,40 @@ public class Main {
 
         System.out.println("Hellow World!");
 
+        squareArea();
+
+        circleArea();
+
+        triangleArea();
+
+    }
+    public static void squareArea() {
+
+        double side = 4.5;
+        double area = (side * side);
+
+        System.out.println("Площадь квадрата равна " + area);
+
+    }
+
+    public static void circleArea() {
+
+        double radius = 5;
+        double pi = 3.14;
+        double area = pi * (radius * radius);
+
+        System.out.println("Площадь круга равна " + area);
+
+    }
+
+    public static void triangleArea() {
+
+        double h = 12;
+        double side = 10.25;
+        double area = (h * side) / 2;
+
+        System.out.println("Площадь треугольника равна " + area);
+
     }
 
 }
